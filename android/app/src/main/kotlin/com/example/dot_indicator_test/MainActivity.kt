3cee1a77a5d8230cd8c226a56ff3c202a1610bdf
farisha <153209114+Farisha13@users.noreply.github.com>
2025-05-19 22:06:27 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/dot_indicator_test/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.dot_indicator_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/homepage.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'pages/page_1.dart';
+import 'pages/page_2.dart';
+import 'pages/page_3.dart';
+import 'pages/page_4.dart';
+
+class HomePage extends StatelessWidget {
+  final _controller = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple[200],
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          // page view
+          SizedBox(
+            height: 500,
+            child: PageView(
+              controller: _controller,
+              children: const [
+                Page1(),
+                Page2(),
+                Page3(),
+                Page4(),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
